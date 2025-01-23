6b43c829e26eeb5537c624d7a6f01625583ea4b7
Oleksandr Savchuk <oleksandr.savchuk@lasoft.org>
2025-01-23 14:09:21 +0200

    add integration tests for sign-in feature

---
+++ src/tests/auth/test_routers.py
@@ -0,0 +1,116 @@
+import pytest
+from fastapi import status
+
+from src.tests.fixtures import *
+from src.auth.utils import create_token
+from src.exceptions import (INVALID_LOGIN_DATA_EXCEPTION, CREDENTIALS_INVALID_EXCEPTION, USER_NOT_FOUND_EXCEPTION,
+                            TOKEN_EXPIRE_EXCEPTION)
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_login_successful(async_client, test_user_with_encrypted_password):
+    login_data = {
+        "username": test_user_with_encrypted_password["email"],
+        "password": "123123"
+    }
+    response = await async_client.post("/auth/login", data=login_data)
+
+    assert response.status_code == status.HTTP_200_OK
+    result = response.json()
+    assert "access_token" in result
+    assert "refresh_token" in result
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_login_invalid_password(async_client, test_user_with_encrypted_password):
+    login_data = {
+        "username": test_user_with_encrypted_password["email"],
+        "password": "wrong_password"
+    }
+    response = await async_client.post("/auth/login", data=login_data)
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json()["detail"] == INVALID_LOGIN_DATA_EXCEPTION.detail
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_login_invalid_email(async_client, test_user_with_encrypted_password):
+    login_data = {
+        "username": 'wrong_email',
+        "password": "123123"
+    }
+    response = await async_client.post("/auth/login", data=login_data)
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json()["detail"] == INVALID_LOGIN_DATA_EXCEPTION.detail
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_login_blank_data(async_client):
+    response = await async_client.post("/auth/login", data={})
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_get_me_successful(async_client, test_user, test_current_user_super_admin):
+    response = await async_client.get("/auth/me")
+    result_user = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    for key in test_user:
+        if key != 'password':
+            assert result_user[key] == result_user[key]
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_get_me_unauthorized(async_client):
+    response = await async_client.get("/auth/me")
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_refresh_token_successful(async_client, test_user):
+    refresh_token = await create_token(test_user["_id"], 15, 'refresh_token')
+    response = await async_client.get("/auth/refresh-token", headers={"refresh-token": refresh_token})
+
+    assert response.status_code == status.HTTP_200_OK
+    result = response.json()
+    assert "access_token" in result
+    assert "refresh_token" in result
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_refresh_token_blank(async_client):
+    response = await async_client.get("/auth/refresh-token")
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json()["detail"] == CREDENTIALS_INVALID_EXCEPTION.detail
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_refresh_token_invalid_user_id(async_client):
+    refresh_token = await create_token(ObjectId(), 15, 'refresh_token')
+    response = await async_client.get("/auth/refresh-token", headers={"refresh-token": refresh_token})
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json()["detail"] == USER_NOT_FOUND_EXCEPTION.detail
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_refresh_token_expired(async_client, test_user):
+    expired_token = await create_token(test_user["_id"], -15, 'refresh_token')
+    response = await async_client.get("/auth/refresh-token", headers={"refresh-token": expired_token})
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json()["detail"] == TOKEN_EXPIRE_EXCEPTION.detail
+
+
+@pytest.mark.asyncio(loop_scope="session")
+async def test_refresh_token_invalid(async_client):
+    invalid_token = "invalid_refresh_token"
+    response = await async_client.get("/auth/refresh-token", headers={"refresh-token": invalid_token})
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json()["detail"] == CREDENTIALS_INVALID_EXCEPTION.detail
--- src/tests/fixtures.py
+++ src/tests/fixtures.py
@@ -7,6 +7,8 @@ from src.main import app
 from src.database import db
 from src.database import user_collection
 from src.users.enums import UserRoles
+from src.auth.utils import get_hashed_password
+from src.auth.deps import get_current_user
 
 fake = Faker()
 
@@ -46,3 +48,35 @@ async def test_users_list():
     users_data = [generate_user_data() for _ in range(3)]
     await user_collection.insert_many(users_data)
     yield users_data
+
+
+@pytest_asyncio.fixture(scope='function')
+async def test_user_with_encrypted_password(test_user):
+    test_user['password'] = await get_hashed_password("123123")
+    hashed_password = {"password": test_user['password']}
+
+    await user_collection.update_one({"_id": test_user["_id"]}, {"$set": hashed_password})
+    yield test_user
+
+
+@pytest_asyncio.fixture(scope='function')
+async def test_current_user(test_user):
+    async def _override_user(role: str):
+        test_user['role'] = role
+        app.dependency_overrides[get_current_user] = lambda: test_user
+        yield
+
+    yield _override_user
+    app.dependency_overrides.clear()
+
+
+@pytest_asyncio.fixture(scope='function')
+async def test_current_user_super_admin(test_current_user):
+    async for _ in test_current_user(UserRoles.super_admin):
+        yield
+
+
+@pytest_asyncio.fixture(scope='function')
+async def test_current_user_prospect(test_current_user):
+    async for _ in test_current_user(UserRoles.prospect):
+        yield
