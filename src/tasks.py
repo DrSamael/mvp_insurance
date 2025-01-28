aca7323d5888a31e56bb31d34f6230df6c414703
Oleksandr Savchuk <oleksandr.savchuk@lasoft.org>
2025-01-28 14:09:23 +0200

    update blacklisted tokens cleanup task

--- src/tasks.py
+++ src/tasks.py
@@ -3,8 +3,9 @@ from src.auth.crud import cleanup_expired_tokens
 
 
 def register_tasks(app):
-    @app.on_event("startup")
-    @repeat_every(seconds=86400)
-    async def scheduled_cleanup():
-        deleted_count = await cleanup_expired_tokens()
-        print(f"Blacklisted tokens cleanup task completed. Deleted {deleted_count} expired tokens.")
+    app.add_event_handler("startup", repeat_every(seconds=86400)(scheduled_cleanup))
+
+
+async def scheduled_cleanup():
+    deleted_count = await cleanup_expired_tokens()
+    print(f"Blacklisted tokens cleanup task completed. Deleted {deleted_count} expired tokens.")
